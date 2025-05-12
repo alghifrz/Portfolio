5a27f6fac27833cb8af3e72ff7127167b8bc8531
alghifrz <alghifarirasyidzola@gmail.com>
2025-05-12 12:20:30 +0700

    analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from '@vercel/analytics/next';
 
 export default function Document() {
   return (
@@ -15,6 +16,7 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   );
